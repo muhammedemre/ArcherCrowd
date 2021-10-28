7dca40ca50a0fcbcb7017050263bf2df39d91c47
UmitKok21 <umit.kok21@gmail.com>
2021-10-28 15:23:26 +0300

    Player, geçiş noktaları ve deniz düzenlendi.

--- Assets/Scripts/Managers/Actors/Officers/Player/PlayerAnimationOfficer.cs
+++ Assets/Scripts/Managers/Actors/Officers/Player/PlayerAnimationOfficer.cs
@@ -7,33 +7,33 @@ public class PlayerAnimationOfficer : MonoBehaviour
     public int currentState = 0;
     public void IdleAnim() 
     {
-        currentState = 0;
-        PlayAnim(0);
+        //currentState = 0;
+        //PlayAnim(0);
     }
 
     public void RunAnim() 
     {
-        currentState = 1;
-        PlayAnim(1);
+        //currentState = 1;
+        //PlayAnim(1);
     }
 
     public void ArrowShootAnim()
     {
-        currentState = 2;
-        PlayAnim(2);
+        //currentState = 2;
+        //PlayAnim(2);
     }
     public void DanceAnim()
     {
-        currentState = 3;
-        PlayAnim(3);
+        //currentState = 3;
+        //PlayAnim(3);
     }
 
     void PlayAnim(int state)
     {     
-        foreach (Transform soldier in PlayerManager.Instance.playerActor.playerSoldierOfficer.soldierContainer)
-        {
-            soldier.GetComponent<SoldierActor>().animator.speed = (1 + (Random.Range(0f, 0.2f)));
-            soldier.GetComponent<SoldierActor>().animator.SetInteger("State", state);
-        }
+        //foreach (Transform soldier in PlayerManager.Instance.playerActor.playerSoldierOfficer.soldierContainer)
+        //{
+        //    soldier.GetComponent<SoldierActor>().animator.speed = (1 + (Random.Range(0f, 0.2f)));
+        //    soldier.GetComponent<SoldierActor>().animator.SetInteger("State", state);
+        //}
     }
 }
